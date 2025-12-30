ccf54f16709a1a49131f15f07c8992937d713d9e
Yafté M. <redyafte@gmail.com>
2025-12-30 01:05:38 -0600

    Remove unwanted files and add them to .gitignore

--- improved_guessing_game/target/debug/deps/improved_guessing_game-e0073a7fcb0cbe80.d
+++
@@ -1,5 +0,0 @@
-/home/redyafte/dev-rust/my_first_steps/improved_guessing_game/target/debug/deps/improved_guessing_game-e0073a7fcb0cbe80.d: src/main.rs
-
-/home/redyafte/dev-rust/my_first_steps/improved_guessing_game/target/debug/deps/libimproved_guessing_game-e0073a7fcb0cbe80.rmeta: src/main.rs
-
-src/main.rs:
