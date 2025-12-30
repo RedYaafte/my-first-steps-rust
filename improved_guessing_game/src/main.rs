c82db82a1afc5a6a3ab4b3516b07b43b6f4d8ac8
RedYaafte <redyafte@gmail.com>
2025-12-30 01:17:22 -0600

    Implement number guessing game logic
    Added random number generation, user input handling, and comparison logic to create a functional number guessing game. The program now prompts the user to guess a number between 1 and 100, provides feedback, and ends when the correct number is guessed.

--- improved_guessing_game/src/main.rs
+++ improved_guessing_game/src/main.rs
@@ -1,3 +1,32 @@
+use rand::Rng;
+use std::io;
+
 fn main() {
     println!("🎮 Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {}", secret_number);
+
+    loop {
+        println!("\n Add your number: ");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number!");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            std::cmp::Ordering::Less => println!("📉 Too small!"),
+            std::cmp::Ordering::Greater => println!("📈 Too big!"),
+            std::cmp::Ordering::Equal => {
+                println!("🎉 You win!");
+                break;
+            }
+        };
+    }
 }
